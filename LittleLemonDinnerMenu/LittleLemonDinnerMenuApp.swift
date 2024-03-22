5ff7bca666ceaaee3b21a65e19a82280ecbd105f
shturkhal <107053994+shturkhal@users.noreply.github.com>
2024-03-22 17:21:48 +0200

    Initial Commit

---
+++ LittleLemonDinnerMenu/LittleLemonDinnerMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonDinnerMenuApp.swift
+//  LittleLemonDinnerMenu
+//
+//  Created by Іван Штурхаль on 22.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonDinnerMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
